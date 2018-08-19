065ec5474acaa69650c06f030fc29a28bfc6aa5c
Soulwolfos <soulwolfos@soulwolfos>
2018-08-19 18:09:54 -0300

    borrado los comentarios por defecto

--- controlMetro/main.cpp
+++ controlMetro/main.cpp
@@ -1,23 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: pipeworkout
- *
- * Created on 19 de agosto de 2018, 18:00
- */
 
 #include <cstdlib>
 
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
 
     return 0;
